2003259ed9dd5d783cd0e202190d30021f013693
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-16 01:15:24 +0500

    Create MainClass.java

---
+++ p4/MainClass.java
@@ -0,0 +1,12 @@
+package p4;
+public class MainClass{
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.getStudentName("Danial");
+        s1.getStudentId(331);
+        s1.getMarks(95);
+        s1.calculateGpa();
+        s1.calculateNumberOfScholorship();
+        s1.info();
+    }
+}
