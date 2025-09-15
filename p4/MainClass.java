b8e9f50603bc7cba691ce07d127b2847b2eac052
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-16 01:36:36 +0500

    Update MainClass.java

--- p4/MainClass.java
+++ p4/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass{
         Student s1 = new Student();
         s1.getStudentName("Danial");
         s1.getStudentId(331);
-        s1.getMarks(95);
+        s1.getMarks(49.6);
         s1.calculateGpa();
         s1.calculateNumberOfScholorship();
         s1.info();
