ef499090bbf9b025ff4fb0e804aefd11e6de9778
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-17 13:45:26 +0500

    Create MainClass.java

---
+++ p5/MainClass.java
@@ -0,0 +1,11 @@
+package p5;
+public class MainClass{
+    public static void main(String args[]){
+        Employee e1 = new Employee();
+        e1.setID(136);
+        e1.setName("Danial Ahmed");
+        e1.setSalary(34.56);
+        e1.setYearsofExperience(23);
+        e1.display();
+    }
+}
