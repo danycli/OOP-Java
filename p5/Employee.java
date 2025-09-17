2ecd1876f802813cbf6e14004da87820ef8bfbcd
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-17 13:45:19 +0500

    Create Employee.java

---
+++ p5/Employee.java
@@ -0,0 +1,42 @@
+package p5;
+
+public class Employee{
+    private int ID;
+    private String Name;
+    private double Salary;
+    private int YearsofExperience;
+
+    public void setID(int id){
+        ID = id;
+    }
+    public void setName(String name){
+        Name = name;
+    }
+    public void setSalary(double salary){
+        Salary = salary;
+    }
+    public void setYearsofExperience(int yearsofexperience){
+        YearsofExperience = yearsofexperience;
+    }
+
+    public int getID(){
+        return ID;
+    }
+    public String getName(){
+        return Name;
+    }
+    public double getSalary(){
+        return Salary;
+    }
+    public int getYearsofExperience(){
+        return YearsofExperience;
+    }
+
+    public void display(){
+        System.out.println("Employee Id = "+ID);
+        System.out.println("Employee name = "+Name);
+        System.out.println("Emplyee Salary = "+Salary);
+        System.out.println("Employee Experiennce "+YearsofExperience+" years");
+    }
+
+}
