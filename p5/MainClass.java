d868f1c64db9752d1e46fbcf5f558b69e3534efb
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-18 23:32:57 +0500

    Update MainClass.java

--- p5/MainClass.java
+++ p5/MainClass.java
@@ -7,5 +7,13 @@ public class MainClass{
         e1.setSalary(34.56);
         e1.setYearsofExperience(23);
         e1.display();
+
+        System.out.println();
+        Employee e2 = new Employee();
+        e2.setID(107);
+        e2.setName("Saad Khan");
+        e2.setSalary(440);
+        e2.setYearsofExperience(20);
+        e2.display();
     }
 }
