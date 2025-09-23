6081de8fcf9b53d0decb4896045f341650272d12
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-24 00:50:38 +0500

    Update passenger.java

--- Uber/passenger.java
+++ Uber/passenger.java
@@ -1,5 +1,27 @@
 package Uber;
 
 public class passenger {
+    private String Name;
+    private String Location;
+    private int MobileNo;
     
+    public void setName(String name){
+        Name = name;
+    }
+    public void setLocation(String location){
+        Location = location;
+    }
+    public void setMobileNo(int number){
+        MobileNo = number;
+    }
+    public String getName(){
+        return Name;
+    }
+    public String getLocation(){
+        return Location;
+    }
+    public int getMobileNo(){
+        return MobileNo;
+    }
+
 }
