9560ba4e413f9f626c2ae430f34aae4337daae7a
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-28 21:03:53 +0500

    Create typecasting.java

---
+++ casting/typecasting.java
@@ -0,0 +1,11 @@
+package casting;
+import java.math.*;
+public class typecasting {
+    public static void main(String [] args){
+        int a = 4;
+        double c = a/7.0;
+        System.out.println(c);
+        System.out.println(Math.max(a,c));
+        
+    }
+}
