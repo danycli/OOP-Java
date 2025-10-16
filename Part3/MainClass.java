88d579eb9b2bce78de57fd829069c292c65353fa
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-10-16 22:26:22 +0500

    Create MainClass.java

---
+++ Part3/MainClass.java
@@ -0,0 +1,53 @@
+package Part3;
+import java.util.Scanner;
+public class MainClass {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        //First Vehichle
+        VehicleReservation[] V = new VehicleReservation[2];
+            System.out.println("Inputing Data for first Vehichle");
+            System.out.print("Enter the Id for the Vehichle :");
+            String id = sc.nextLine();
+            System.out.print("Enter the Name of the owner :");
+            String name = sc.nextLine();
+            System.out.print("Enter Vehichle Type :");
+            String type = sc.nextLine();
+            System.out.print("Enter the total time Vehichle spend in the parkingLot :");
+            int time = sc.nextInt();
+            System.out.print("Enter the ParkingSlot Id :");
+            int slot = sc.nextInt();
+
+            V[0] = new VehicleReservation(id, name, type, slot);
+            V[0].setTime(time);
+            V[0].VehicleExitAndCalculateBill();
+            V[0].DisplayInfo();
+
+            //Second Vehichle
+            System.out.println("\nInputing Data for second Vehichle");
+            sc.nextLine();
+            System.out.print("Enter the Id for the Vehichle :");
+            String id2 = sc.nextLine();
+            System.out.print("Enter the Name of the owner :");
+            String name2 = sc.nextLine();
+            System.out.print("Enter Vehichle Type :");
+            String type2 = sc.nextLine();
+            System.out.print("Enter the total time Vehichle spend in the parkingLot :");
+            int time2 = sc.nextInt();
+            System.out.print("Enter the ParkingSlot Id :");
+            int slot2 = sc.nextInt();
+
+            V[1] = new VehicleReservation(id2, name2, type2, slot2);
+            V[1].setTime(time2);
+            V[1].VehicleExitAndCalculateBill();
+            V[1].DisplayInfo();
+
+            boolean result = VehicleReservation.CompareVehiclesSlots(V[0],V[1]);
+            if(result == true){
+                System.out.println("The Vehicles are beside eachother");
+            }else{
+                System.out.println("The Vehichles are not beside eachother");
+            }
+
+            sc.close();
+    }
+}
