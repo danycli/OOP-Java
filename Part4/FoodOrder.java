6fc413468a3fc92543a8d5f124373c824734a703
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-10-17 18:33:51 +0500

    resturant

--- Part4/FoodOrder.java
+++ Part4/FoodOrder.java
@@ -1,5 +1,134 @@
 package Part4;
 
 public class FoodOrder {
-    
+    private int orderId;
+    private String CustomerName;
+    private String PhoneNumber;
+    private String foodType; //Drink or Food
+    private String delivery;// Standard or Fast
+    private float TotalDistance;
+    private String OrderType;//DineIn, Site Order
+    private float TableTax;
+    private float deliveryCharges; 
+    private float litre;
+    private int numOfDrinks;
+    private float Tax;
+    private float Bill;
+    static private int totalOrders = 0;
+
+    //Setters
+    void setOd(int id){
+        this.orderId = id;
+    }
+    void setName(String name){
+        CustomerName = name;
+    }
+    void setNumber(String number){
+        PhoneNumber = number;
+    }
+    void setFoodType(String Ftype){
+        this.foodType = Ftype;
+    }
+    void setDelivery(String delivery){
+        this.delivery = delivery;
+    }
+    void setOrderType(String Otype){
+        this.OrderType = Otype;
+    }
+    void setDistance(float distance){
+        this.TotalDistance = distance;
+    }
+    //Getters
+    int getId(){
+        return orderId;
+    }
+    String getName(){
+        return CustomerName;
+    }
+    String getNumber(){
+        return PhoneNumber;
+    }
+    String getFoodType(){
+        return foodType;
+    }
+    String getDeliveryType(){
+        return delivery;
+    }
+    String getOrderType(){
+        return OrderType;
+    }
+    float getDistance(){
+        return TotalDistance;
+    }
+    float getDeliveryCharges(){
+        return deliveryCharges;
+    }
+    float getTableTax(){
+        return TableTax;
+    }
+    float getTax(){
+        return Tax;
+    }
+    float getBill(){
+        return Bill;
+    }
+    int getTotalOrders(){
+        return totalOrders;
+    }    
+
+    //Constructors
+    FoodOrder(int id, String Ftype, String DeliveryType, float distance, String name, String number, String OrderType){ //Constructor for Home Delivery
+        orderId = id;
+        foodType = Ftype;
+        delivery = DeliveryType;
+        TotalDistance = distance;
+        CustomerName = name;
+        PhoneNumber = number;
+        this.OrderType = OrderType;
+    }
+    FoodOrder(int id, String Ftype,String name, String number, String OrderType){ //For Dine-In Order
+        orderId = id;
+        foodType = Ftype;
+        CustomerName = name;
+        PhoneNumber = number;
+        this.OrderType = OrderType;
+    }
+
+    //Other Methods
+
+    //Method for delicery Charges
+    void DeliveryCharges(){
+        if(TotalDistance <= 2.5){
+            deliveryCharges = 250;
+        }else{
+            deliveryCharges = 450;
+        }
+    }
+    //Method For TableTax
+    private void tableTax(int bill){
+        if(bill < 2500){
+            TableTax = (4.2f/100)*bill;
+        }else{
+            TableTax = (6f/100)*bill;
+        }
+    }
+    //Method for regular Tax
+    private void tax(int bill){
+        if(bill < 1500){
+            Tax = (1.2f/100)*bill;            
+        }else{
+            Tax = (3.5f/100)*bill;  
+        }
+    }
+    //Method for Total Bill
+    // void TotalBill(){
+    //     int bill;
+    //     if(OrderType == "Dine-In"){
+
+    //         bill = 
+    //     }else{
+
+    //     }
+    // }
+
 }
--- Part4/MainClass.java
+++ Part4/MainClass.java
@@ -1,5 +1,53 @@
 package Part4;
-
+import java.util.Scanner;
 public class MainClass {
-    
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choose Order Type \n1.Dine-In\n2.Home Delivery\nChoose :");
+        int orderType = sc.nextInt();
+        System.out.print("Enter your Name :");
+        String name = sc.nextLine();
+        System.out.print("Enter your Phone Number :");
+        String number = sc.nextLine();
+
+        System.out.print("Choose Food Type :\n1.Drink\2.Food\nChoose :");
+        int FoodType = sc.nextInt(); 
+        while(FoodType < 1 || FoodType > 2){
+            System.out.print("Invalid Choice!\nTry Again (1-2) :");
+            FoodType = sc.nextInt();
+        }
+        if(FoodType == 1){
+            System.out.println("Choose the Drink Of your Choice \n1.Coca Cola\n2.Pepsi\n3.Sprite\n4.7up\n5.Marinda");
+            int drink = sc.nextInt();
+            while(drink < 1 || drink > 5){
+                System.out.print("Invalid Choice!\nTry Again (1-5) :");
+                drink = sc.nextInt();
+            }
+            System.out.print("Choose Drink Size\n1. 1Litre\n2. 1.5Litre \n3. 2Litre\n 4. 2.25Litre\nChoose :");
+            int litre = sc.nextInt();
+            System.out.println("Choose the number of drinks (Max 10):");
+            int nums = sc.nextInt();
+            while(nums < 1 || nums > 10){
+                System.out.print("Invalid Choice!\nTry Again (1-5) :");
+                nums = sc.nextInt();
+            }
+            if(litre == 1){
+
+            }
+        }
+        
+        String foodName ;
+        if(FoodType == 1){
+            foodName = "Drink"; 
+        }else{
+            foodName = "Food"; 
+        }
+        String Type;
+        if(orderType == 1){
+            Type = "Dine-In";
+        }else{
+            Type = "Home Delivery";
+        }
+        FoodOrder F1 = new FoodOrder(12,foodName,name,number,Type);
+    }
 }
