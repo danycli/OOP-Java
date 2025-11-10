7a981c6ff46c37589abe5fda6559b47adffda767
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-11-10 12:38:34 +0500

    d

--- String/WordsInAStentence.java
+++ String/WordsInAStentence.java
@@ -1,7 +1,7 @@
 package String;
 import java.util.Scanner;
 public class WordsInAStentence {
-    public static void mani(String [] args){
+    public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String[] sp = s.split(" ");
