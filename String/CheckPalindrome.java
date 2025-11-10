79b2602c89fc4485eac5e53cd5d0ceff1492a911
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-11-10 12:36:02 +0500

    words

--- String/CheckPalindrome.java
+++ String/CheckPalindrome.java
@@ -4,11 +4,11 @@ public class CheckPalindrome {
     public static void main(String [] args){
         String abc = "A man a plan a canal Panama";
         boolean b = true;
-        abc = abc.replace(" ", "");
-        abc = abc.toLowerCase();
+        abc = abc.replace(" ", "").toLowerCase();
         for(int i = 0; i < abc.length() / 2; i++){
             if(abc.charAt(i) != abc.charAt(abc.length() - 1 - i)){
                 b = false;
+                break;
             }
         }
         if(b == false){
---
+++ String/WordsInAStentence.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.Scanner;
+public class WordsInAStentence {
+    public static void mani(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] sp = s.split(" ");
+        System.out.println("Total words count = "+sp.length);
+        sc.close();
+    }
+}
