bb4b69c9ada5f5087dc7e32550f477a4e9090555
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-12-06 16:24:08 +0500

    Composition

--- Assignmet04/Program2/MainClass.java
+++ Assignmet04/Program2/MainClass.java
@@ -2,7 +2,7 @@ package Assignmet04.Program2;
 
 public class MainClass {
     public static void main(String [] args){
-        Machine m1 = new Machine(12, 10);
+        Machine m1 = new Machine(12, 2);
 
         m1.calculateTotalSensorsValue();
         System.out.println((m1.checkSensorOutput() == true) ? "All Sensors are of same type" : "All sensors are not of same type");
