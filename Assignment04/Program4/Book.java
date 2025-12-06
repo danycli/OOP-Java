030749fad120ec5c3b9520bb2dc81d73b405ea40
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-12-06 16:38:23 +0500

    progress

--- Assignment04/Program4/Book.java
+++ Assignment04/Program4/Book.java
@@ -1,5 +1,52 @@
 package Assignment04.Program4;
 
 public class Book {
-    
+    private int id;
+    private String title;
+    private String author;
+    private float price;
+    private float discountedPrice;
+    //Constructor
+    public Book(int id, String title, String author, float price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    //Getters
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public float getDiscountedPrice() {
+        return discountedPrice;
+    }
+    //Discount
+    public void calculateDiscountedPrice(){
+        discountedPrice = price * 0.90f;
+    }
 }
--- Assignment04/Program4/Magazine.java
+++ Assignment04/Program4/Magazine.java
@@ -1,5 +1,57 @@
 package Assignment04.Program4;
 
 public class Magazine {
-    
+    private int id;
+    private String title;
+    private int IssueNumber;
+    private int year;
+    private float price;
+    private float discountedPrice;
+    //Constructor
+    public Magazine(int id, String title, int issueNumber, int year, float price) {
+        this.id = id;
+        this.title = title;
+        IssueNumber = issueNumber;
+        this.year = year;
+        this.price = price;
+    }
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIssueNumber(int issueNumber) {
+        IssueNumber = issueNumber;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    //Getters
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getIssueNumber() {
+        return IssueNumber;
+    }
+    public int getYear() {
+        return year;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public float getDiscountedPrice() {
+        return discountedPrice;
+    }
 }
